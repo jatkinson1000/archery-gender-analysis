12614d085cbff3ef0f511394f789abffe83bdbad
jatkinson1000 <109271713+jatkinson1000@users.noreply.github.com>
2023-08-05 16:30:22 +0100

    Restructure into a packaged format.

---
+++ archery-gender-analysis/__init__.py
@@ -0,0 +1,8 @@
+"""Package with utilities for performing data analysis of archery competitions."""
+from archery-gender-analysis import general_routines, ianseo_scrape, plotting
+
+__all__ = [
+    "general_routines",
+    "ianseo_scrape",
+    "plotting",
+]
